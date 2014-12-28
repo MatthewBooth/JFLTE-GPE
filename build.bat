80a0d2014611fc495194e364812e7602c6d5a512
Kryten2k35 <Kryten2k35@gmail.com>
2014-12-28 05:52:06 +0000

    Wrong way around slash for Windows. Windows has them thisaway

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 ::UltimaROM build script
 set /p VERSION=<version.txt
 del Danvdh-%VERSION%.zip
-tools\7za.exe a -mx9 -xr@tools/exclusion.txt Danvdh-%VERSION%.zip META-INF system root data boot.img
+tools\7za.exe a -mx9 -xr@tools\exclusion.txt Danvdh-%VERSION%.zip META-INF system root data boot.img
 cd ..
