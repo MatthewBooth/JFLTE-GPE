b7edd79cdef84cf28f493214b7396c30c9577f06
Kryten2k35 <Kryten2k35@gmail.com>
2014-12-28 05:30:05 +0000

    Updated build scripts. Fresh build each time

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 ::UltimaROM build script
 set /p VERSION=<version.txt
-tools\7za.exe a -up0q0r2x2y2z1w2 -mx9 -xr@tools/exclusion.txt Danvdh-%VERSION%.zip META-INF system root data boot.img
+del Danvdh-%VERSION%.zip
+tools\7za.exe a -mx9 -xr@tools/exclusion.txt Danvdh-%VERSION%.zip META-INF system root data boot.img
 cd ..
