45dbc126640e50bea63c5776c0f3e98f589d073f
Kryten2k35 <Kryten2k35@gmail.com>
2014-12-29 16:56:20 +0000

    Changed filename

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VERSION=$(cat version.txt)
-FILE=Danvdh-${VERSION}.zip
+FILE=JFLTE-GPE-5.0-${VERSION}.zip
 rm "$FILE"
 7za a -mx9 -xr@tools/exclusion.txt "$FILE" META-INF system root data boot.img Changelog.md
 cd ..
