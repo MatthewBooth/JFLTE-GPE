847a34993cb86d4372650f4d0809cc22ae4cf577
Kryten2k35 <Kryten2k35@gmail.com>
2014-12-29 17:40:53 +0000

    Left boot.img in the build script by mistake

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 VERSION=$(cat version.txt)
 FILE=JFLTE-GPE-5.0-${VERSION}.zip
 rm "$FILE"
-7za a -mx9 -xr@tools/exclusion.txt "$FILE" META-INF system root data kernel boot.img Changelog.md
+7za a -mx9 -xr@tools/exclusion.txt "$FILE" META-INF system root data kernel Changelog.md
 cd ..
