09c1f32564604cebcf5cc13b55c5765d81d6af1d
Kryten2k35 <Kryten2k35@gmail.com>
2014-12-29 03:34:42 +0000

    Ported CM's changelog into the GPE settings apk

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 VERSION=$(cat version.txt)
 FILE=Danvdh-${VERSION}.zip
 rm "$FILE"
-7za a -mx9 -xr@tools/exclusion.txt "$FILE" META-INF system root data boot.img
+7za a -mx9 -xr@tools/exclusion.txt "$FILE" META-INF system root data boot.img Changelog.md
 cd ..
