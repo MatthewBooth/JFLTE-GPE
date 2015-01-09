94572d1eb0f30d97ff7a71033c51ad01de080985
Kryten2k35 <Kryten2k35@gmail.com>
2015-01-09 21:55:06 +0000

    Updated OTA app to 2.1.1 - Added some translations and fixed some minor issues

--- aroma/scripts/set_dpi.sh
+++ aroma/scripts/set_dpi.sh
@@ -2,6 +2,6 @@
 
 # Change the DPI based on the parameters
 
-sed -i "s/ro.sf.lcd_density=480/ro.sf.lcd_density=${1}/g" "/system/build.prop"
+sed -i "s/ro.sf.lcd_density=480/ro.sf.lcd_density=$1/g" "/system/build.prop"
 sync
 exit 0
