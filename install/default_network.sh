6123ced6310ab136580847992aebe7a5629430ab
Kryten2k35 <Kryten2k35@gmail.com>
2015-06-24 01:28:14 +0100

    Lollipop VZW now uses default Network 10

--- install/default_network.sh
+++ install/default_network.sh
@@ -2,7 +2,7 @@
 
 # Change default network for CMDA users
 
-sed -i "s/ro.telephony.default_network=9/ro.telephony.default_network=8/g" "/system/build.prop"
+sed -i "s/ro.telephony.default_network=9/ro.telephony.default_network=10/g" "/system/build.prop"
 sed -i "s/#telephony.lteOnCdmaDevice=1/telephony.lteOnCdmaDevice=1/g" "/system/build.prop"
 sync
 exit 0
